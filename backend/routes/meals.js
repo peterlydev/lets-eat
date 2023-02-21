fa44f9477dcd82f12352e1587b5b5c8e1b8bc8c9
Peter Ly <peter.ly1@hotmail.com>
2023-02-21 23:38:02 +0000

    created routes

--- backend/routes/meals.js
+++ backend/routes/meals.js
@@ -1,9 +1,30 @@
+const express = require('express')
+
+const router = express.Router();
+
 //get all meals
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET ALL MEALS'})
+});
 
 //get single meal
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET A SINGLE MEAL'})
+})
 
 //post a meal
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST A NEW MEAL'})
+})
 
 // delete a meal
+router.delete('/', (req, res) => {
+    res.json({mssg: 'DELETE A MEAL'})
+})
 
 //update a meal
+router.patch('/', (req, res) => {
+    res.json({mssg: 'UPDATE A MEAL'})
+})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mealRoutes = require('./routes/meals')
 
 app.use(express.json())
 
@@ -8,6 +9,8 @@ app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 });
+
+app.use('/api/meals', mealRoutes)
 
 app.get('/', function (req, res) {
   res.json({mssg: "Welcome to the Let's Eat app!"})
