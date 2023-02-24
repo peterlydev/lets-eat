d3ce274b2821b37a4fbbe6637ee3b4f0401acce3
Peter Ly <peter.ly1@hotmail.com>
2023-02-24 23:21:09 +0000

    Connected Mongoose to server

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
-const mealRoutes = require('./routes/meals')
+const mealRoutes = require("./routes/meals");
 
-app.use(express.json())
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method);
+  next();
 });
 
-app.use('/api/meals', mealRoutes)
+app.use("/api/meals", mealRoutes);
 
-app.get('/', function (req, res) {
-  res.json({mssg: "Welcome to the Let's Eat app!"})
-});
-
-
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
-});
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Connected to DB & Listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
